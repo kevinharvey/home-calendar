d203588b3a150b824e23003ca54ff5f739ce5f5f
Kevin Harvey <kevin@storyandstructure.com>
2018-03-29 00:00:41 -0500

    Initial commit of project w/React and webpack

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # home-calendar
-The Harveys' home calendar
+The Harveys' home calendar, based on https://blog.daftcode.pl/react-calendar-with-google-calendar-as-cms-tutorial-5f5d81e425a9
+
+Built with [react-big-calendar](https://github.com/intljusticemission/react-big-calendar/)
---
+++ source/app.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { render } from 'react-dom'
+import moment from 'moment'
+
+import BigCalendar from 'react-big-calendar'
+BigCalendar.momentLocalizer(moment)
+
+import { getEvents } from './gcal'
+
+require('style-loader!css-loader!react-big-calendar/lib/css/react-big-calendar.css')
+
+class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      events: []
+    }
+  }
+
+  componentDidMount () {
+    getEvents((events) => {
+      this.setState({events})
+    })
+  }
+
+  render () {
+    return (
+      <BigCalendar
+        style={{height: '420px'}}
+        events={this.state.events}
+      />
+    )
+  }
+}
+
+render(<App />, document.getElementById('root'))
